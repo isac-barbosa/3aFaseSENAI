cde083f550e58a069ddc350ba321b2270689888e
isac-barbosa <santosisac196@gmail.com>
2026-04-07 22:07:57 -0300

    Atualizei pouca coisa aqui

--- Dev-FrontEnd/multi-pages/src/layouts/Footer/index.jsx
+++ Dev-FrontEnd/multi-pages/src/layouts/Footer/index.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className='bg-amber-900'>
-      <h3>Footer</h3>
+    <footer className='bg-gray-200  flex  justify-around items-center h-12 mt-auto'>
+      <h3 className=''>Github isac-barbosa</h3>
+      <a href="">  © 2025 Sua Farmárcia - Todos os direitos reservados</a>
     </footer>
   )
 }
--- Dev-FrontEnd/multi-pages/src/layouts/Main/index.jsx
+++ Dev-FrontEnd/multi-pages/src/layouts/Main/index.jsx
@@ -7,7 +7,7 @@ export const Main = () => {
     return (
         <div className='flex min-h-screen flex-col'>
             <Header />
-            <main className='flex-1 conainer mx-auto'>
+            <main className='flex-1 container mx-auto'>
                 <Outlet />
             </main>
             <Footer />

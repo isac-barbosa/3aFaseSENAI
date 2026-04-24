eab25c530abff2632f92d27ab8e88ed45891f007
isac-barbosa <santosisac196@gmail.com>
2026-04-24 19:25:30 -0300

    Só falta Services e Controller de prontuário

--- Dev-BackEnd/clinica_API/src/routes/consulta.ts
+++ Dev-BackEnd/clinica_API/src/routes/consulta.ts
@@ -1,73 +1,28 @@
 import { Router } from "express";
-import { createHash } from "../utils/createHash";
-import { prisma } from "../prisma/prisma";
-import type { Consulta } from "../prisma/generated/prisma/client";
-import bcrypt from "bcrypt";
+import { consultaController } from "../controllers/ConsultaController";
 
 
 export const consultaRouter = Router();
 
 consultaRouter.get('/consulta', async (_, res) => {
-    const consulta = await prisma.consulta.findMany();
-    res.json(consulta);
+    return consultaController.listarConsultas(_, res)
 })
 
 consultaRouter.get('/consulta/:id', async (req, res) => {
-    const idConsulta = Number(req.params.id)
-    const consulta = await prisma.consulta.findUnique({
-        where: {
-            id: idConsulta
-        }
-    })
-    return res.status(200).json(consulta)
+    return consultaController.buscarConsultaId(req, res)
 })
 
 
 consultaRouter.post("/consulta", async (req, res) => {
-    console.log(req.body)
-    const dadosConsulta = req.body as Consulta
-    const consultaCriada = await prisma.consulta.create({
-        data: {
-            motivo: dadosConsulta.motivo,
-            data_consulta: dadosConsulta.data_consulta,
-            observacoes: dadosConsulta.observacoes,
-            medico_responsavel_id: dadosConsulta.medico_responsavel_id,
-            paciente_id: dadosConsulta.paciente_id   
-        }
-    })
-    return res.status(201).json(consultaCriada)
+    return consultaController.criarConsulta(req, res)
 })
 
 
 consultaRouter.put('/consulta/:id', async (req, res) => {
-    const idConsulta = Number(req.params.id)
-    const dadosParaAtualizar = req.body as Omit<Consulta, 'id'>
-    await prisma.consulta.update({
-        data: {
-            ...dadosParaAtualizar
-        },
-        where: {
-            id: idConsulta
-        }
-    })
-    const consultaAtualizado = await prisma.consulta.findUnique({
-        where: {
-            id: idConsulta
-        }
-    })
-    return res.status(200).json(consultaAtualizado)
+    return consultaController.atualizarConsulta(req, res)
 })
 
 
 consultaRouter.delete('/consulta/:id', async (req, res) => {
-    const idConsulta = Number(req.params.id)
-    const deletarConsulta = await prisma.consulta.delete({
-        where: {
-            id: idConsulta
-        }
-    })
-    return res.status(200).json({
-        message: "Usuário deletado com sucesso",
-        data: deletarConsulta
-    })
+    return consultaController.deletarConsulta(req, res)
 })
--- Dev-BackEnd/clinica_API/src/routes/usuario.ts
+++ Dev-BackEnd/clinica_API/src/routes/usuario.ts
@@ -1,72 +1,28 @@
 import { Router } from "express";
-import { createHash } from "../utils/createHash";
-import { prisma } from "../prisma/prisma";
-import type { Usuario } from "../prisma/generated/prisma/client";
-import bcrypt from "bcrypt";
+import { usuarioController } from "../controllers/UsuárioController";
 
 
 export const usuarioRouter = Router();
 
 usuarioRouter.get('/usuarios', async (_, res) => {
-    const usuarios = await prisma.usuario.findMany();
-    res.json(usuarios);
+    return usuarioController.listarUsuarios(_, res)
 })
 
 usuarioRouter.get('/usuarios/:id', async (req, res) => {
-    const idUsuario = Number(req.params.id)
-    const usuario = await prisma.usuario.findUnique({
-        where: {
-            id: idUsuario
-        }
-    })
-    return res.status(200).json(usuario)
+    return usuarioController.buscarUserId(req, res)
 })
 
 
 usuarioRouter.post("/usuarios", async (req, res) => {
-    console.log(req.body)
-    const dadosUsuario = req.body as Usuario
-    const hash = await createHash(dadosUsuario.senha,);
-    const usuarioCriado = await prisma.usuario.create({
-        data: {
-            email: dadosUsuario.email,
-            nome: dadosUsuario.nome || null,
-            senha: hash
-        }
-    })
-    return res.status(201).json(usuarioCriado)
+    return usuarioController.criarUsuario(req, res)
 })
 
 
 usuarioRouter.put('/usuarios/:id', async (req, res) => {
-    const idUsuario = Number(req.params.id)
-    const dadosParaAtualizar = req.body as Omit<Usuario, 'id'>
-    await prisma.usuario.update({
-        data: {
-            ...dadosParaAtualizar
-        },
-        where: {
-            id: idUsuario
-        }
-    })
-    const usuarioAtualizado = await prisma.usuario.findUnique({
-        where: {
-            id: idUsuario
-        }
-    })
-    return res.status(200).json(usuarioAtualizado)
+    return usuarioController.atualizarUser(req, res)
 })
 
 
 usuarioRouter.delete('/usuarios/:id', async (req, res) => {
-    const idUsuario = Number(req.params.id)
-    const deletarUsuario = await prisma.usuario.delete({
-        where: {
-            id: idUsuario
-        }
-    })
-    return res.status(200).json({
-        message: "Usuário deletado com sucesso",
-        data: deletarUsuario
-    })
+    return usuarioController.deletarUser(req, res)
 })

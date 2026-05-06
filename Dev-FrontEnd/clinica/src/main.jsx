f9901c6eaa8fde8a7e59c168dcb46ec337516deb
isac-barbosa <santosisac196@gmail.com>
2026-05-05 21:43:41 -0300

    TPegar o resto do professor

--- Dev-FrontEnd/clinica/src/main.jsx
+++ Dev-FrontEnd/clinica/src/main.jsx
@@ -12,6 +12,8 @@ import 'react-toastify/dist/ReactToastify.css'
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import DashboardLayout from './layouts/DashboardLayout';
 
 
 const router = createBrowserRouter([
@@ -21,8 +23,16 @@ const router = createBrowserRouter([
   },
   {
     path: "/dashboard",
-    element: <Dashboard />
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      { path: "/dashboard", element: <Dashboard /> }
+    ]
   }
+
 ]);
 
 createRoot(document.getElementById('root')).render(

63f92c8f96853ec83b5719ff501a3fc69a520701
isac-barbosa <santosisac196@gmail.com>
2026-05-19 11:50:09 -0300

    ajustando coisas da aula passada

--- Dev-FrontEnd/clinica/src/components/RegisterUser/index.jsx
+++ Dev-FrontEnd/clinica/src/components/RegisterUser/index.jsx
@@ -1,51 +1,59 @@
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
+
 import axios from 'axios'
 
 const RegisterUser = () => {
 
-    //estados de controle dos campos
+    // estados de controle dos campos
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
 
     //funções que alteram o valor dos estados
-    const handlEmailChange = (e) => setEmail(e.target.value)
-    const handlPasswordChange = (e) => setPassword(e.target.value)
-    const handlconfirmPasswordChange = (e) => setConfirmPassword(e.target.value)
+    const handleEmailChange = (e) => setEmail(e.target.value)
+    const handlePasswordChange = (e) => setPassword(e.target.value)
+    const handleConfirmPasswordChange = (e) => setConfirmPassword(e.target.value)
 
-    //estados (match password e validação do botão de salvar)
+    // estados (match password e validação do botão de salvar)
     const [isPasswordMatch, setIsPasswordMatch] = useState(true)
+
     const [isSaving, setIsSaving] = useState(false)
 
-    //validação do match
+    // validação do match 
+
     const isPasswordValid = () => password.length >= 8 && password === confirmPassword
+
     const resetForm = () => {
         setEmail('')
         setPassword('')
         setConfirmPassword('')
         setIsPasswordMatch(true)
     }
+
     const handleSubmit = async (e) => {
         e.preventDefault()
+
         if (!isPasswordValid()) {
             setIsPasswordMatch(false)
             return
         }
+
         setIsSaving(true)
 
         try {
             await axios.post('http://localhost:3000/users', {
                 email, password
             })
+
             setIsSaving(false)
             resetForm()
-            toast.success("Usuário criado com sucesso", {
+            toast.success('Usuário Criado com Sucesso!', {
                 autoClose: 2000,
-                hideProgressBar: true              
+                hideProgressBar: true
             })
-        }catch (error){
-            console.log("Erro ao criar usuário", error)
+        } catch (error) {
+            console.error('Erro ao criar usuário', error)
             toast.error('Erro ao criar o usuário!', {
                 autoClose: 2000,
                 hideProgressBar: true
@@ -53,63 +61,70 @@ const RegisterUser = () => {
             setIsSaving(false)
         }
 
+
     }
 
+
     return (
-        <div className='w-full max-w-md p-6 bg-white roundedx1 shadow-lg'>
-            <h2 className='text-2x1 font-bold mb-6 text-center'>Criar Usuário</h2>
-            <form onSubmit={handleSubmit} >
+        <div className='w-full max-w-md p-6 bg-white rounded-xl'>
+            <h2 className='text-2xl font-bold mb-6 text-center'>Criar Usuário</h2>
+            <form onSubmit={handleSubmit}>
                 <fieldset>
-                    <label htmlFor="email" className='block text-sm font-medium mb-1'>Email:</label>
+                    <label htmlFor='email' className='block text-sm font-medium mb-1'>Email:</label>
                     <input
-                        type="email"
+                        type='email'
                         id='email'
                         value={email}
-                        onChange={handlEmailChange}
+                        onChange={handleEmailChange}
                         required
-                        minLength={8}
-                        className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                        className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+                    />
                 </fieldset>
 
                 <fieldset>
-                    <label htmlFor="password" className='block text-sm font-medium mb-1'>Senha:</label>
+                    <label htmlFor='password' className='block text-sm font-medium mb-1'>Senha:</label>
                     <input
-                        type="password"
+                        type='password'
                         id='password'
                         value={password}
-                        onChange={handlPasswordChange}
+                        onChange={handlePasswordChange}
                         required
                         minLength={8}
-                        className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' />
+                        className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+                    />
                 </fieldset>
 
                 <fieldset>
-                    <label htmlFor="confirmPassword" className='block text-sm font-medium mb-1'>confirmar senha:</label>
+                    <label htmlFor='confirmPassword' className='block text-sm font-medium mb-1'>Confirmar Senha:</label>
                     <input
-                        type="password"
+                        type='password'
                         id='confirmPassword'
                         value={confirmPassword}
-                        onChange={handlconfirmPasswordChange}
+                        onChange={handleConfirmPasswordChange}
                         required
                         minLength={8}
-                        className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' 
-                        />
-                        {!isPasswordMatch && (
-                            <p className='text-red-500 text-sm mt-1'>As senhas não correspondem</p>
-                        )}
+                        className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+                    />
+
+                    {!isPasswordMatch && (
+                        <p className='text-red-500 text-sm mt-1'>As senhas não correspondem</p>
+                    )}
                 </fieldset>
 
                 <div>
                     <button
                         type='submit'
-                        disabled = {isSaving}
-                        className={`w-full p-2 rounded-lg text-white ${isSaving ? 'bg-gray-400 cursor-not-allowed': 'bg-blue-600 hover:bg-blue-700 cursor-pointer'} transition-colors`}
-                        >
-                        {isSaving ? "Salvando...": "Criar Usuário"}
-                        {/* {isSaving ? "Salvando ...": "Criar Usuário"} */}
-                        
+                        disabled={isSaving}
+                        className={`w-full p-2 rounded-lg text-white mt-4 ${isSaving ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-700 cursor-pointer'
+                            } transition-colors`}
+                    >
+                        {isSaving ? "Salvando ..." : "Criar Usuário"}
+
                     </button>
                 </div>
+
+
+
             </form>
         </div>
     )
--- Dev-FrontEnd/clinica/src/components/SideMenu/index.jsx
+++ Dev-FrontEnd/clinica/src/components/SideMenu/index.jsx
@@ -1,14 +1,20 @@
-import { Link, useNavigate } from 'react-router'
+import { Link, useNavigate, NavLink } from 'react-router'
 import {
     MdDashboard,
     MdExitToApp,
     MdMenu,
     MdClose
 } from 'react-icons/md'
-// import { faUserPlus, FaListAlt, FaCalendarCheck } from 'react-icons/fa'
+
+import {
+    FaUserPlus,
+    FaListAlt,
+    FaCalendarCheck
+} from 'react-icons/fa'
+
 import { useAuth } from '../../contexts/AuthContext'
 import { useState } from 'react'
-import { FaCalendarCheck, FaListAlt, FaUserPlus } from 'react-icons/fa'
+
 
 
 const SideMenu = () => {
@@ -16,10 +22,10 @@ const SideMenu = () => {
 
     const { logout } = useAuth()
 
-    //controle do menu sanfonado
+    // controle do menu sanfonado
     const [isCollapsed, setIsCollapsed] = useState(false)
 
-    //função do logout 
+    // função do logout
     const handleLogout = () => {
         logout()
         navigate('/')
@@ -32,83 +38,100 @@ const SideMenu = () => {
     }
 
 
-    return (
-        <aside 
-        className={`h-screen bg-cyan-800 text-white flex flex-col justify-between transition-all duration-300 ${
-            isCollapsed ?'w-20':'w-64'
-        }`}
-        >
 
+    return (
+        <aside
+            className={`h-screen bg-cyan-800 text-white flex flex-col justify-between transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-64'
+                }`}
+        >
             {/* topo - botão toggle */}
-            <div className='p-4 flex items-center justify-between border-b border bg-cyan-700'>
+            <div className='p-4 flex items-center justify-between border-b border-cyan-700'>
                 {
                     !isCollapsed && (
-                        <h1 className='text-lg font-bold'> Clinica +</h1>
+                        <h1 className='text-lg font-bold'>Clínica +</h1>
                     )
                 }
-                <button 
-                onClick={toggleMenu}
-                className='text-white hover:text-cyan-300 focus: outline-none'
+                <button
+                    onClick={toggleMenu}
+                    className='text-white hover:text-cyan-300 focus:outline-none'
                 >
-                    {isCollapsed ? <MdMenu size={24}/>: <MdClose size={24}/>}
+                    {isCollapsed ? <MdMenu size={24} /> : <MdClose size={24} />}
                 </button>
             </div>
 
-            {/*Menu */}
+            {/* Menu */}
             <nav className='flex-1 p-4 space-y-4 overflow-y-auto'>
                 <ul className='space-y-3'>
                     <li>
-                        <Link
-                        to='/dashboard'
-                        className='flex items-center gap-3 hover:text-cyan-300'
+                        <NavLink
+                            to="/dashboard"
+                            className={({ isActive }) =>
+                                `flex gap-2 hover:text-cyan-300 ${isActive ? "text-cyan-300" : "text-white"
+                                }`
+                            }
                         >
                             <MdDashboard size={20} />
                             {!isCollapsed && <span>Início</span>}
-                        </Link>
+                        </NavLink>
+
+
                     </li>
-                      <li>
-                        <Link
-                        to='/pacientes'
-                        className='flex items-center gap-3 hover:text-cyan-300'
+                    <li>
+                        <NavLink
+                            to="/pacientes"
+                            className={({ isActive }) =>
+                                `flex gap-2 hover:text-cyan-300 ${isActive ? "text-cyan-300" : "text-white"
+                                }`
+                            }
                         >
                             <FaUserPlus size={20} />
-                            {!isCollapsed && <span> Pacientes </span>}
-                        </Link>
+                            {!isCollapsed && <span>Pacientes</span>}
+                        </NavLink>
+
                     </li>
-                      <li>
-                        <Link
-                        to='/consultas'
-                        className='flex items-center gap-3 hover:text-cyan-300'
+                    <li>
+                        <NavLink
+                            to="/consultas"
+                            className={({ isActive }) =>
+                                `flex gap-2 hover:text-cyan-300 ${isActive ? "text-cyan-300" : "text-white"
+                                }`
+                            }
                         >
-                            <FaCalendarCheck size={20} />
-                            {!isCollapsed && <span> Consultas </span>}
-                        </Link>
+                            <MdMenu size={20} />
+                            {!isCollapsed && <span>Consultas</span>}
+                        </NavLink>
+
+
                     </li>
-                      <li>
-                        <Link
-                        to='/exames'
-                        className='flex items-center gap-3 hover:text-cyan-300'
+                    <li>
+                        <NavLink
+                            to="/exames"
+                            className={({ isActive }) =>
+                                `flex gap-2 hover:text-cyan-300 ${isActive ? "text-cyan-300" : "text-white"
+                                }`
+                            }
                         >
                             <FaListAlt size={20} />
-                            {!isCollapsed && <span> Exames </span>}
-                        </Link>
+                            {!isCollapsed && <span>Exames</span>}
+                        </NavLink>
+
                     </li>
                 </ul>
             </nav>
-                {/* botão sair */}
-                <div className='p-4 border-t border-cyan-700'>
-                    <button 
+
+            {/* botao sair */}
+            <div className='p-4 border-t border-cyan-700'>
+                <button
                     onClick={handleLogout}
                     className='flex items-center gap-3 text-red-300 hover:text-red-500 w-full cursor-pointer'
-                    
-                    >
-                        <MdExitToApp size={20}/>
-                        {!isCollapsed && <span> Sair </span>}
+                >
+                    <MdExitToApp size={20} />
+                    {!isCollapsed && <span>Sair</span>}
 
+                </button>
+            </div>
 
-                    </button>
-                </div>
-        </aside>
+        </aside >
     )
 }
 

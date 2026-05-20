b2e291ebc3365f90f4495768422ea4c801f92e6c
isac-barbosa <santosisac196@gmail.com>
2026-05-19 21:35:25 -0300

    terminar atividade proposta pelo professor

--- Dev-FrontEnd/clinica/src/components/PatientsList/index.jsx
+++ Dev-FrontEnd/clinica/src/components/PatientsList/index.jsx
@@ -57,7 +57,47 @@ const PatientsList = () => {
 
 
     return (
-        <div>PatientsList</div>
+        <div className="bg-white shadow rounded-2xl p-6 mt-8">
+            <h2 className="text-xl font-semibold text-cyan-800 mb-4">
+                Informações Rápidas de Pacientes
+            </h2>
+            {/* {Campo de busca} */}
+            <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-4 gap-3 ">
+                <label htmlFor="search" className="tet-gray-700 font-medium">
+                    Buscar Paciente:
+                </label>
+                <input type="text" 
+                        id="search"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                        placeholder="Digite o nome, email ou telefone "
+                        className="border rounded-lg px-3 py-2 w-full sm:w-80 focus: ring-2 focus:ring-cyan-600 outline-none"
+                        
+                        />
+            </div>
+
+            {/* {Lista de pacientes} */}
+            {
+                filteredPatients.length > 0 ? (
+                    <ul className="divide-y divide-gray-200">
+                        {
+                            filteredPatients.map((patient) =>(
+                                <li
+                                    key={patient.id}
+                                    className="flex flex-col sm:flex-row sm:items-center justify-between py-4 "
+                                    >
+                                        
+
+                                    </li>
+                            ))
+                        }
+
+                    </ul>
+                )
+                    
+                
+            }
+        </div>
     )
 }
 
